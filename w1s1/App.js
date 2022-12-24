dcae1a71486b1fa42ba337b64fdf5773a0f97820
Kunal Sharma <sharmakunal14.work@gmail.com>
2022-12-24 22:09:16 +0530

    w1s1

---
+++ w1s1/App.js
@@ -0,0 +1,9 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const ele1 = React.createElement(
+	"h1",
+	{ id: "header", style: { fontWeight: "150" }, className: ["icon", "text"] },
+	"Hello1"
+);
+const ele2 = React.createElement("h1", { id: "header" }, "Hello2");
+const cont = React.createElement("div", { id: "container" }, [ele1, ele2]);
+root.render(cont);
