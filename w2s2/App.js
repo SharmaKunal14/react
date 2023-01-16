c8acbbb08802da70acb7bf25a5ec6823e5e57c10
Kunal Sharma <sharmakunal14.work@gmail.com>
2023-01-16 11:03:47 +0530

    w2s2

---
+++ w2s2/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const ele1 = React.createElement(
+	"h1",
+	{ id: "header", style: { fontWeight: "150" }, className: ["icon", "text"] },
+	"Hello1"
+);
+const ele2 = React.createElement("h1", { id: "header" }, "Hello2");
+const cont = React.createElement("div", { id: "container" }, [ele1, ele2]);
+
+const Title = () => {
+	return <h1>Title</h1>;
+};
+const Header = () => {
+	return (
+		<header>
+			<h1>Header</h1>
+			{<Title />}
+		</header>
+	);
+};
+root.render(<Header />);
