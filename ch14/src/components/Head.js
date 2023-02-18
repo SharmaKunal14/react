96250c0a189f5175278321f8117f492b82608c95
Kunal Sharma <sharmakunal14.work@gmail.com>
2023-02-19 01:43:48 +0530

    add search bar and auto-suggest feature

--- ch14/src/components/Head.js
+++ ch14/src/components/Head.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
+import { YOUTUBE_SEARCH_SUGGESTIONS } from "../utils/constants";
 const Head = () => {
+	const [searchQuery, setSearchQuery] = useState("");
+	const [suggestions, setSuggestions] = useState([]);
+	const [showSuggestions, setShowSuggestions] = useState(false);
+
+	useEffect(() => {
+		const timer = setTimeout(() => getSearchSuggestions(searchQuery), 200);
+
+		return () => clearTimeout(timer);
+	}, [searchQuery]);
+
+	const getSearchSuggestions = async (searchQuery) => {
+		const suggestions = await fetch(
+			YOUTUBE_SEARCH_SUGGESTIONS + searchQuery
+		);
+		const [query, resp] = await suggestions.json();
+		setSuggestions(resp);
+	};
 	const dispatch = useDispatch();
 	const handleToggleEvent = () => {
 		dispatch(toggleMenu());
@@ -23,15 +41,34 @@ const Head = () => {
 					/>
 				</a>
 			</div>
-			<div className="col-span-10 px-10 text-center">
+			<div className="col-span-10 px-10 text-center relative">
 				<input
 					type="text"
 					className="border border-gray-500 rounded-l-full w-1/2 px-4 py-2"
+					value={searchQuery}
+					onChange={(e) => setSearchQuery(e.target.value)}
+					onFocus={() => setShowSuggestions(true)}
+					onBlur={() => setShowSuggestions(false)}
 				/>
 				<button className="btn p-2 border border-gray-500 bg-gray-200 rounded-r-full">
 					🔍
 				</button>
+				{showSuggestions && (
+					<div className="absolute bg-white translate-x-[20rem] translate-y-[0.1rem] shadow-lg px-2 py-2 rounded-lg z-10">
+						<ul>
+							{suggestions.map((suggestion) => (
+								<li
+									className="w-[41rem] text-left p-2 hover:bg-gray-100"
+									key={suggestion}
+								>
+									🔍 {suggestion}
+								</li>
+							))}
+						</ul>
+					</div>
+				)}
 			</div>
+
 			<div className="col-span-1">
 				<img
 					src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
